31a93cbcf13a5cf87afcf118e366e601820ffdf5
RogerCodes <rogelioherreradev@gmail.com>
2024-05-20 14:27:14 -0600

    Made changes to UI, description and box shadows, also removed description on project modals

--- resources/scripts.js
+++ resources/scripts.js
@@ -8,7 +8,7 @@ function ShowModal(buttonclicked) {
   Swal.fire({
     title: `<h2>Project Demo: ${Title}</>`,
     html: `
-        <p>${Description}</p>
+        <p>${Description == undefined ? "" : Description}</p>
         <div class="project-modal-container">
         <span class="project-modal-container-iframe-loader"></span>
         <iframe id="project-modal-container-iframe" width="420" height="315"
@@ -28,27 +28,22 @@ function GetData(buttonId) {
   switch (buttonId) {
     case "button-project-onlinedice":
       Title = "Online Dice";
-      Description = "Online Dice";
       VideoUrl = "https://www.youtube.com/embed/VK2kjvcI6n0?autoplay=0&mute=0";
       break;
     case "button-project-ledCube":
       Title = "LED Cube";
-      Description = "LED Cube project";
       VideoUrl = "https://www.youtube.com/embed/HsLrH-nLg5Y?autoplay=0&mute=0";
       break;
     case "button-project-LeigthLive":
       Title = "Leigth Live";
-      Description = "Leigth.Live project";
       VideoUrl = "https://www.youtube.com/embed/XP0uQcrqBQM?autoplay=0&mute=0";
       break;
     case "button-project-RogerCodes":
       Title = "RogerCodes Blog";
-      Description = "RogerCodes.Blog project";
       VideoUrl = "https://www.youtube.com/embed/jNQXAC9IVRw?autoplay=0&mute=0";
       break;
     case "button-project-ControlPracticas":
       Title = "Control Practicas";
-      Description = "Control Practicas project";
       VideoUrl = "https://www.youtube.com/embed/5gTn-QREIqA?autoplay=0&mute=0";
       break;
     default:
